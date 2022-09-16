c77f0442d23574ea68ceeefaedef7ff6c1286d53
LucasLimaDM <lucas.limadm10@gmail.com>
2022-09-16 17:15:54 -0300

    teste_veloc

---
+++ js_2/index.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log('a velocidade do seu carro é ' + vel + 'km/h')
+if (vel > 60) {
+    console.log('Você foi multado por passar do limite de velocidade')
+}
+else {
+    console.log('Parabéns!')
+}
